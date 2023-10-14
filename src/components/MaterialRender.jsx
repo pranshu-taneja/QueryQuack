78138cdcbb3c95b73f8d925f66f2aaae453f8b83
pranshu-taneja <pranshu45taneja@gmail.com>
2023-10-14 23:30:12 +0530

    Optimized the loading images & responsiveness

--- src/components/MaterialRender.jsx
+++ src/components/MaterialRender.jsx
@@ -2,6 +2,7 @@ import React, { useMemo, useEffect, useState } from "react";
 import { MaterialReactTable } from "material-react-table";
 import csvtojson from "csvtojson";
 import useTableTheme from "./styles/useTableTheme";
+import "./styles/MaterialRender.css"
 import { ThemeProvider } from "@mui/material";
 
 export default function MaterialRender({ tableURL }) {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,17 +6,26 @@ import { useNavigate } from "react-router-dom";
 
 function Navbar() {
   const navigate = useNavigate();
-  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  const [isDropDownOpen, setIsDropDownOpen] = useState(true);
   const [isMobileView, setisMobileView] = useState(false);
 
   useEffect(() => {
     function findMobileView() {
       if (window.innerWidth < 768) {
         setisMobileView(true);
-        console.log("Mobile View Active!!");
-      } 
+        setIsDropDownOpen(false);
+      } else {
+        setisMobileView(false);
+        setIsDropDownOpen(true);
+      }
     }
     findMobileView();
+
+    window.addEventListener("resize", findMobileView);
+
+    return () => {
+      window.removeEventListener("resize", findMobileView);
+    };
   }, []);
 
   const handleDropDown = () => {
@@ -39,16 +48,17 @@ function Navbar() {
             <img src={logo_URL} alt="logoIcon" className="logoIcon"></img>
             <p className="logoText">QueryQuack</p>
           </div>
-          <div
+          {!isMobileView ? null : <div className="Hamburger">&#9776;</div>}
+          {/* <div
             style={{ display: isMobileView ? "flex" : "none" }}
             className="Hamburger"
           >
             &#9776;
-          </div>
+          </div> */}
         </section>
 
         <section
-          style={{ display: isDropDownOpen ? "none" : "flex" }}
+          style={{ display: isDropDownOpen ? "flex" : "none" }}
           className="secondary-section"
         >
           <Link to={"/home"}>HOME</Link>
@@ -64,66 +74,3 @@ function Navbar() {
 }
 
 export default Navbar;
-
-// import React, { useState, useEffect } from "react";
-// import "./styles/Navbar.css";
-// import { logo_URL, user_icon_URL } from "../helpers/Constants";
-// import { Link } from "react-router-dom";
-// import { useNavigate } from "react-router-dom";
-
-// function Navbar() {
-//   const navigate = useNavigate();
-//   const [isDropDownOpen, setIsDropDownOpen] = useState(true);
-//   const [isMobileView, setIsMobileView] = useState(false);
-
-//   useEffect(() => {
-//     // Update the isMobileView state based on window width
-//     const handleResize = () => {
-//       setIsMobileView(window.innerWidth < 768); // Adjust the threshold as needed
-//     };
-
-//     // Initial check on component mount
-//     handleResize();
-
-//     // Listen for window resize events
-//     window.addEventListener("resize", handleResize);
-
-//     // Clean up the event listener when the component is unmounted
-//     return () => {
-//       window.removeEventListener("resize", handleResize);
-//     };
-//   }, []);
-
-//   return (
-//     <div className="navComponent-container">
-//       <div className="navbar-container">
-//         <section
-//           onClick={() => {
-//             setIsDropDownOpen(!isDropDownOpen);
-//           }}
-//           className="primary-section"
-//         >
-//           <div className="logoIcon-Container">
-//             <img src={logo_URL} alt="logoIcon" className="logoIcon"></img>
-//             <p className="logoText">QueryQuack</p>
-//           </div>
-//           {!isMobileView ? null : <div className="Hamburger">&#9776;</div>}
-//         </section>
-
-//         <section
-//           style={{ display: isMobileView && isDropDownOpen ? "none" : "flex" }}
-//           className="secondary-section"
-//         >
-//           <Link to={"/home"}>HOME</Link>
-//           <Link to={"/editor"}>EDITOR</Link>
-//           <Link className="userIcon-container ">
-//             <img src={user_icon_URL} className="userIcon" alt="userIcon" />
-//             <div className="userName">USER NAME</div>
-//           </Link>
-//         </section>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Navbar;
